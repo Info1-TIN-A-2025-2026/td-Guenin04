268b74f63717e21782ce176da847dca4008b7018
Guenin04 <noe.guenin@heig-vd.ch>
2025-09-16 12:32:28 +0000

    added TD 20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23;
+	int account_value = -42; // signed optionnel devant
+	float temp = 18.1;
+	double pi = 3.1415;
+}
