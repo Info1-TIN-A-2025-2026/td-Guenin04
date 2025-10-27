f32299cfcd4302730ad669ec3a97efdb5d08316e
Guenin04 <noe.guenin@heig-vd.ch>
2025-10-27 14:10:14 +0000

    Added today's td on operators

---
+++ TD20251027 - operator/td.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, const char *argv[])
+{
+	/*
+	// Declare and Initialize variables
+	int a = 0;
+	int b = 0;
+	int q = 0;
+	int r = 0;
+
+	// Ask the user for input values
+	printf("Input two integers: ");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	// Calculate quotient and remainder
+	q = a / b;
+	r = a % b;
+	printf("a/b = %d\n", q);
+	printf("a%%b = %d\n", r);
+
+	// Demonstration of post and pre-increment operators
+	a = 10;;
+	printf("a = %d\n", a++);
+	a= 10;
+	printf("a = %d\n", ++a);
+
+	// Overflow demonstration with signed char
+	char x = 127;
+	printf("x = %d\n", ++x);
+
+	// Left shift assignment operator demonstration
+	char a = 42;
+	printf("a = %d\n", a<<=1);
+
+	// Declare and Initialize variables
+	unsigned char x = 42;
+	unsigned char y = 117;
+	unsigned char z = 0;
+	// Logical AND operator demonstration
+	z = x & y;
+	printf("z = %d\n", z);
+
+	// Logical XOR operator demonstration
+	z = x ^ y;
+	printf("z = %d\n", z);
+
+	// Logical OR operator demonstration
+	z = x | y;
+	printf("z = %d\n", z);
+
+	// Logical NOT operator demonstration
+	x = ~x;
+	printf("~x = %d\n", x);
+	y = ~y;
+	printf("~b = %d\n", y);
+
+	// Declare and Initialize variables
+	double x = 0.0;
+	int result = 0;
+	// Comparaison and Logical operators demonstration
+	printf("Input a value: ");
+	scanf("%lf", &x);
+	result = ((x>=2.0)&&(x<=4.0))||((x>=7)&&(x<=11));
+	printf("result = %d\n", result);
+	
+	// Declare and Initialize variables
+	int x = 0;
+	int y = 0;
+	int result = 0;
+	// Ask the user for input values
+	printf("Input values of x and y : ");
+	scanf("%d", &x);
+	scanf("%d", &y);
+	// Comparaison and Logical NOT operator demonstration
+	result = !(((y >= -1) && (y <= 5)) && ((x >= -2) && (x <= 6)));
+	printf("result = %d\n", result);
+	
+
+	// Declare and Initialize variables
+	int x = 0;
+	int result = 0;
+	// Ternary operator demonstration
+	printf("Input value of x to check if even: ");
+	scanf("%d", &x);
+	result = !(x%2) ? 1 : 0;
+	printf("result = %d\n", result);
+
+	*/
+	return 0;
+}
