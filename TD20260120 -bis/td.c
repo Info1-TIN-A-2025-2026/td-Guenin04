e9a7d6cfa655490a1ba0f8795d2401fba498a240
Guenin04 <noe.guenin@heig-vd.ch>
2026-01-20 13:04:55 +0000

    added second td on loops

---
+++ TD20260120 -bis/td.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(int argc, const char *argv[])
+{
+	//normal for
+	for (int i = 0; i < 10;i++)
+	{
+		printf("%d\n",i);
+		//to get out
+		// if(condition){
+			// break;
+		// }
+
+		//to get back up the loop
+		// if(condition){
+			// continue;
+		// }
+	}
+	//infinite for
+	for (;;)
+	{
+		//to get out
+		// if(condition){
+			// break;
+		// }
+
+		//to get back up the loop
+		// if(condition){
+			// continue;
+		// }
+	}
+
+	//infinite while loop
+	while(1){
+		//to get out
+		// if(condition){
+			// break;
+		// }
+
+		//to get back up the loop
+		// if(condition){
+			// continue;
+		// }
+	}
+
+	//infinite do while
+	do{
+		//to get out
+		// if(condition){
+			// break;
+		// }
+
+		//to go check condition of do while
+		// if(condition){
+			// continue;
+		// }
+	} while (1);
